1641f9fd4423eb3d4c01a60ceae54ef7a1f0c314
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-03-13 19:57:17 -0500

    footer and header

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<footer class="footer">
+    <div class="footer-content">
+        <h3>COFFEE</h3>
+        <p>Put a coffee in every hand.</p>
+    </div>
+    <div class="footer-bottom">
+          <p>copyright &copy;2023 COFFEE. designed by <span>Juan</span></p>
+    </div>
+</footer>
+</body>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<header class="header">
+  <a href="#default" class="title">COFEE</a>
+  <div class="hright">
+    <a class="active" href="#home">Home</a>
+    <a href="#contact">About</a>
+    <a href="#about">Sign In/Up</a>
+  </div>
+</header>
+</body>
