a39b9e2de624dd3f89fa10e8c280df15762c3a66
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-03-17 17:17:58 -0500

    fixes

--- landing.php
+++ landing.php
@@ -1,22 +1,22 @@
+<?php 
+  include('includes/header.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <link rel="stylesheet" href="landingstyle.css">
 </head>
 
-<?php 
-  include('includes/header.php');
-?>
-
-
-<div class="background">
-    <div class = "cont">
-        <button class = "button-1">
-            View our menu
-        </button>
-    </div>
-    <img class = "backimg" src="assets/coback.jpeg" alt="coffe image">
-</div>
+<html>
+  <div class="background">
+      <div class = "cont">
+          <button class = "button-1">
+              View our menu
+          </button>
+      </div>
+      <img class = "backimg" src="assets/coback.jpeg" alt="coffe image">
+  </div>
+</html>
 
  
 <?php 
