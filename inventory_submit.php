68cd089ff3be1dd13f7b85618e178623f2600b94
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-03-22 13:44:38 -0500

    register

--- inventory_submit.php
+++ inventory_submit.php
@@ -1,49 +1,45 @@
 <?php
-	// Connect to the database
-	$servername = "coffee-shop.mysql.database.azure.com";
-	$username = "group9";
-	$password = "Databases9";
-	$dbname = "pointofsales";
+$servername = "coffee-shop.mysql.database.azure.com";
+$username = "group9";
+$password = "Databases9";
+$dbname = "pointofsales";
 
-	$conn = mysqli_init();
-    mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+// Create connection
+$con = mysqli_init();
+mysqli_real_connect($con, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+// Check connection
+if ($con->connect_error) {
+	die("Connection failed: " . $con->connect_error);
+}
 
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+// Get form data
+$productid = mysqli_real_escape_string($con, $_POST['productid']);
+$item_name = mysqli_real_escape_string($con, $_POST['item_name']);
+$price = mysqli_real_escape_string($con, $_POST['price']);
+$quantity = mysqli_real_escape_string($con, $_POST['quantity']);
 
-	// Check if form is received
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		// Clean input
-		$productid = mysqli_real_escape_string($conn, $_POST["productid"]);
-		$item_name = mysqli_real_escape_string($conn, $_POST["item_name"]);
-		$price = mysqli_real_escape_string($conn, $_POST["price"]);
-		$quantity = mysqli_real_escape_string($conn, $_POST["quantity"]);
+// Check if item already exists
+$sql = "SELECT * FROM inventory WHERE productid = '$productid'";
+$result = $con->query($sql);
 
-		// Validating before insertion
-		if (!is_numeric($productid) || strlen($productid) != 7) {
-			die("Invalid Product ID");
-		}
-		if (empty($item_name)) {
-			die("Item Name cannot be empty");
-		}
-		if (!is_numeric($price) || $price <= 0) {
-			die("Invalid Price");
-		}
-		if (!is_numeric($quantity) || $quantity <= 0) {
-			die("Invalid Quantity");
-		}
+if ($result->num_rows > 0) {
+	// Item already exists, update quantity
+	$sql = "UPDATE inventory SET quantity = quantity + '$quantity' WHERE productid = '$productid'";
+	if ($con->query($sql) === TRUE)
+	header("Location: index.php");
+	exit();
+} else {
+// Item does not exist, add new item
+$sql = "INSERT INTO inventory (productid, item_name, price, quantity) VALUES ('$productid', '$item_name', '$price', '$quantity')";
+if ($con->query($sql) === TRUE) {
+	header("Location: index.php");
+	exit();
+} else {
+	$error = "Error: " . $sql . "<br>" . $con->error;
+	header("Location: index.php?error=$error");
+	exit();
+}
+}
 
-		// insertion
-		$sql = "INSERT INTO inventory (productid, item_name, price, quantity)
-				VALUES ('$productid', '$item_name', $price, $quantity)";
-
-		if ($conn->query($sql) === TRUE) {
-			echo "Stock added successfully";
-		} else {
-			echo "Error: " . $sql . "<br>" . $conn->error;
-		}
-	}
-    //closing connection
-	$conn->close();
+$con->close();
 ?>
---
+++ inventoryregister.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Inventory Item</title>
+	<style>
+		input[type="text"], input[type="number"], input[type="submit"] {
+			padding: 5px;
+			font-size: 18px;
+			margin: 5px;
+			border-radius: 5px;
+			border: 1px solid #ccc;
+		}
+		input[type="submit"] {
+			background-color: #4CAF50;
+			color: white;
+			cursor: pointer;
+		}
+		input[type="submit"]:hover {
+			background-color: #3e8e41;
+		}
+		.error {
+			color: red;
+			font-size: 16px;
+		}
+	</style>
+</head>
+<body>
+	<h2>Add Inventory Item</h2>
+	<form method="post" action="inventory_submit.php">
+		<label for="productid">Product ID:</label>
+		<input type="text" name="productid" id="productid" pattern="[0-9]{7}" title="Please enter a 7-digit number." required><br>
+		<label for="item_name">Item Name:</label>
+		<input type="text" name="item_name" id="item_name" maxlength="45" required><br>
+		<label for="price">Price:</label>
+		<input type="number" name="price" id="price" step="0.01" min="0" required><br>
+		<label for="quantity">Quantity:</label>
+		<input type="number" name="quantity" id="quantity" min="0" required><br>
+		<input type="submit" value="Add Item">
+	</form>
+	<?php if(isset($_GET['error'])) { ?>
+		<p class="error"><?php echo $_GET['error']; ?></p>
+	<?php } ?>
+</body>
+</html>
