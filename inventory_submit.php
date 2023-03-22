16925e05abc9bbfdfa1fc30f7b928867a81694a8
Juan Yanez <65358942+juanzenay@users.noreply.github.com>
2023-03-22 17:21:09 -0500

    Add files via upload

--- inventory_submit.php
+++ inventory_submit.php
@@ -1,49 +1,53 @@
 <?php
-	// Connect to the database
-	$servername = "coffee-shop.mysql.database.azure.com";
-	$username = "group9";
-	$password = "Databases9";
-	$dbname = "pointofsales";
+//shows any errors on the script
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//
 
-	$conn = mysqli_init();
-    mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+// db connections
+$servername = "coffee-shop.mysql.database.azure.com";
+$username = "group9";
+$password = "Databases9!";
+$dbname = "pointofsales";
 
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+$con = mysqli_init();
+mysqli_ssl_set($con, NULL, NULL, '/path/to/mysql-ca.pem', NULL, NULL);
+mysqli_real_connect($con, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
+}
 
-	// Check if form is received
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		// Clean input
-		$productid = mysqli_real_escape_string($conn, $_POST["productid"]);
-		$item_name = mysqli_real_escape_string($conn, $_POST["item_name"]);
-		$price = mysqli_real_escape_string($conn, $_POST["price"]);
-		$quantity = mysqli_real_escape_string($conn, $_POST["quantity"]);
+// Get form data
+$productid = mysqli_real_escape_string($con, $_POST['productid']);
+$item_name = mysqli_real_escape_string($con, $_POST['item_name']);
+$price = mysqli_real_escape_string($con, $_POST['price']);
+$quantity = mysqli_real_escape_string($con, $_POST['quantity']);
 
-		// Validating before insertion
-		if (!is_numeric($productid) || strlen($productid) != 7) {
-			die("Invalid Product ID");
-		}
-		if (empty($item_name)) {
-			die("Item Name cannot be empty");
-		}
-		if (!is_numeric($price) || $price <= 0) {
-			die("Invalid Price");
-		}
-		if (!is_numeric($quantity) || $quantity <= 0) {
-			die("Invalid Quantity");
-		}
 
-		// insertion
-		$sql = "INSERT INTO inventory (productid, item_name, price, quantity)
-				VALUES ('$productid', '$item_name', $price, $quantity)";
 
-		if ($conn->query($sql) === TRUE) {
-			echo "Stock added successfully";
-		} else {
-			echo "Error: " . $sql . "<br>" . $conn->error;
-		}
-	}
-    //closing connection
-	$conn->close();
+// Check if item already exists
+$sql = "SELECT * FROM inventory WHERE productid = '$productid'";
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+	// Item already exists, update quantity
+	$sql = "UPDATE inventory SET quantity = quantity + '$quantity' WHERE productid = '$productid'";
+	if ($con->query($sql) === TRUE)
+	header("Location: inventoryregister.php");
+	exit();
+} else {
+// Item does not exist, add new item
+$sql = "INSERT INTO inventory (productid, item_name, price, quantity) VALUES ('$productid', '$item_name', '$price', '$quantity')";
+if ($con->query($sql) === TRUE) {
+	header("Location: inventoryregister.php");
+	exit();
+} else {
+	$error = "Error: " . $sql . "<br>" . $con->error;
+	header("Location: inventoryregister.php?error=$error");
+	exit();
+}
+}
+
+$con->close();
 ?>
