0c140e05c4cefee02e839d97b9348a0712ce16e6
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-04-05 16:58:06 -0500

    buttons

--- includes/employeeheader.php
+++ includes/employeeheader.php
@@ -8,7 +8,7 @@
   <a href="#default" class="title">Bean Me Up</a>
   <div class="hright">
     <a class="active" href="#home" onclick="window.location.href = 'employeehome.php';">Home</a>
-    <a href="#contact" onclick="window.location.href = 'inventoryregister.php';">Add Inventory</a>
+    <a href="#contact" onclick="window.location.href = 'inventoryregister.php';">Add/View Inventory</a>
     <a href="#about" onclick="window.location.href = 'logout.php';">Sign Out</a>
   </div>
 </header>
