5182e1ffc29e9e899d77657c850f8947373a52f8
Yayan57 <adriangarza115@yahoo.com>
2023-04-10 12:14:09 -0500

    commit, and page for orderstock

--- orderstock.php
+++ orderstock.php
@@ -22,38 +22,8 @@
     $result = $con->query($sql);
 
 
-    if ($result->num_rows > 0) {
-      echo "<table>";
-      echo "<tr><th>Product ID</th><th>Product Name</th><th>Price</th><th>Quantity</th><th>Order Quantity</th><th></th></tr>";
-      while ($row = $result->fetch_assoc()) {
-        echo "<form method='post' action='".$_SERVER['PHP_SELF']."'>";
-        echo "<tr>";
-        echo "<td>".$row['product_id']."</td>";
-        echo "<td>".$row['item_name']."</td>";
-        echo "<td>".$row['price']."</td>";
-        echo "<td>".$row['quantity']."</td>";
-        echo "<td><input type='number' name='quantity' value='1' min='1' max='".$row['quantity']."'></td>";
-        echo "<td><input type='hidden' name='product_id' value='".$row['product_id']."'><input type='submit' name='order_button' value='Order'></td>";
-        echo "</tr>";
-        echo "</form>";
-      }
-      echo "</table>";
-    } else {
-      echo "No products found.";
-    }
-
     // Process orders
     if (isset($_POST['order_button'])) {
-      $sql = "UPDATE inventory SET quantity = quantity + '$quantity' WHERE productid = '$productid'";
-    if ($con->query($sql) === TRUE) {
-        header("Location: stock.php");
-        exit();
-    } else {
-        $error = "Error: " . $sql . "<br>" . $con->error;
-        header("Location: stock.php?error=$error");
-        exit();
-    }
-
       // Check if there is enough stock
       $sql = "SELECT * FROM inventory WHERE branchnum = $branch_number AND product_id='$product_id'";
       $row = $result->fetch_assoc();
@@ -71,10 +41,21 @@
           echo "Error updating record: " . mysqli_error($conn);
         }
       }
+    if ($con->query($sql) === TRUE) {
+        header("Location: inventory.php");
+        exit();
+    } else {
+        $error = "Error: " . $sql . "<br>" . $con->error;
+        header("Location: inventory.php?error=$error");
+        exit();
+    }
+
+      
+      
     }
 
     // Check if inventory is low
-    $sql = "SELECT * FROM inventory WHERE branchnum = $branch_number AND quantity < 10";
+    $sql = "SELECT * FROM inventory WHERE branchnum = $branch_number AND quantity < 3";
     $result = mysqli_query($conn, $sql);
 
     if ($result->num_rows > 0) {
@@ -98,81 +79,6 @@
 		<?php 
 		  include('includes/footer.php');
 		?>
-
-<?php 
-	session_start();
-	if(isset($_SESSION['type']) and $_SESSION['type'] == "manager"){
-	include('includes/managerheader.php');    
-	}else{
-	include('includes/header.php');
-	}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Inventory</title>
-</head>
-<style>
-    /* CSS for the inventory page */
-table {
-  border-collapse: collapse;
-  width: 100%;
-  margin-bottom: 20px;
-}
-
-table, th, td {
-  border: 1px solid black;
-}
-
-th, td {
-  padding: 10px;
-}
-
-th {
-  background-color: #ccc;
-}
-
-input[type="text"] {
-  padding: 10px;
-  border: 1px solid black;
-}
-
-button[type="submit"] {
-  padding: 10px;
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-  cursor: pointer;
-}
-
-button[type="submit"]:hover {
-  background-color: #3e8e41;
-}
-
-</style>
-<body>
-	<h1>Inventory</h1>
-	<form method="post" action="inventory.php">
-		<label for="search">Search item:</label>
-		<input type="text" id="search" name="search">
-		<button type="submit">Search</button>
-	</form>
-	<table>
-		<thead>
-			<tr>
-				<th>Product ID</th>
-				<th>Item Name</th>
-				<th>Price</th>
-				<th>Quantity</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php include('inventoryp.php'); ?>
-		</tbody>
-	</table>
-</body>
-</html>
 
 
 <?php 
---
+++ orderstockpage.php
@@ -0,0 +1,67 @@
+<?php 
+	session_start();
+	if(isset($_SESSION['type']) and $_SESSION['type'] == "customer"){
+	include('includes/headeruser.php');    
+	}
+	else if(isset($_SESSION['type']) and $_SESSION['type'] == "employee"){
+	include('includes/employeeheader.php');    
+	}else{
+	include('includes/header.php');
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Order Inventory Item</title>
+	<style>
+		input[type="text"], input[type="number"], input[type="submit"] {
+			padding: 5px;
+			font-size: 18px;
+			margin: 5px;
+			border-radius: 5px;
+			border: 1px solid #ccc;
+		}
+		input[type="submit"] {
+			background-color: #4CAF50;
+			color: white;
+			cursor: pointer;
+		}
+		input[type="submit"]:hover {
+			background-color: #3e8e41;
+		}
+		.error {
+			color: red;
+			font-size: 16px;
+		}
+	</style>
+</head>
+<body>
+	<h2>Order/View Inventory Items</h2>
+	<form method="post" action="orderstock.php">
+		<label for="productid">Product ID:</label>
+		<input type="text" name="productid" id="productid" pattern="[0-9]{7}" title="Please enter a 7-digit number." required><br>
+		<label for="item_name">Item Name:</label>
+		<input type="text" name="item_name" id="item_name" maxlength="45" required><br>
+		<label for="price">Price:</label>
+		<input type="number" name="price" id="price" step="0.01" min="0" required><br>
+		<label for="quantity">Quantity:</label>
+		<input type="number" name="quantity" id="quantity" min="0" required><br>
+		<label for="branchnum">Branch Number:</label>
+		<input type="text" name="branchnum" id="branchnum" pattern="[0-9]{3}" title="Please enter a 3-digit number." required><br>
+
+		<input type="submit" value="order_button">
+
+	</form>
+
+	<input type="submit" value="View Inventory" href="#about" onclick="window.location.href = 'inventory.php';">
+
+	<?php if(isset($_GET['error'])) { ?>
+		<p class="error"><?php echo $_GET['error']; ?></p>
+	<?php } ?>
+</body>
+</html>
+
+<?php 
+  include('includes/footer.php');
+?>
