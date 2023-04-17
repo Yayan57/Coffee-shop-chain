6b2b5362db9c43924a9fad72468ac15a88f0a1b3
Juan Yanez <65358942+juanzenay@users.noreply.github.com>
2023-04-17 17:29:21 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 // db connections
 $servername = "coffee-shop.mysql.database.azure.com";
 $username = "group9";
@@ -15,44 +17,70 @@ if ($con->connect_error) {
   die("Connection failed: " . $con->connect_error);
 }
 
-// Process cart
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  // Insert transaction details
-  $payment_total = 0;
-  foreach ($_POST['item'] as $item_id) {
-    $item_query = "SELECT * FROM inventory WHERE productid='".$item_id."'";
-    $item_result = mysqli_query($con, $item_query);
-    $item_row = mysqli_fetch_assoc($item_result);
-    $payment_total += $item_row['price'];
-  }
-  $payment_type = $_POST['payment_type'];
-  $date = date("Y-m-d");
-  $time = date("Y-m-d H:i:s");
-  $to_go = $_POST['to_go'];
-  $branchN = $_POST['branchN'];
-  $transaction_query = "INSERT INTO transaction_details (customer_user, payment_total, payment_type, date, time, to_go, branchN) VALUES ('".$_SESSION['username']."', '".$payment_total."', '".$payment_type."', '".$date."', '".$time."', '".$to_go."', '".$branchN."')";
-  mysqli_query($con, $transaction_query);
-  $transaction_id = mysqli_insert_id($con);
-
-  // Insert transaction items and update inventory
-  foreach ($_POST['item'] as $item_id) {
-    $quantity = 1;
-    $transit_id = $transaction_id;
-    $item_query = "SELECT * FROM inventory WHERE productid='".$item_id."'";
-    $item_result = mysqli_query($con, $item_query);
-    $item_row = mysqli_fetch_assoc($item_result);
-    $product_id = $item_row['productid'];
-    $transaction_item_query = "INSERT INTO transaction_items (product_id, quantity, transit_id) VALUES ('".$product_id."', '".$quantity."', '".$transit_id."')";
-    mysqli_query($con, $transaction_item_query);
-    
-    // Update inventory
-    $inventory_quantity = $item_row['quantity'];
-    $new_inventory_quantity = $inventory_quantity - $quantity;
-    $inventory_update_query = "UPDATE inventory SET quantity='".$new_inventory_quantity."' WHERE productid='".$product_id."'";
-    mysqli_query($con, $inventory_update_query);
-  }
-
-  // Display confirmation message
-  echo "<h1>Thank you for your order!</h1>";
+// Initialize cart
+if (!isset($_SESSION['cart'])) {
+  $_SESSION['cart'] = array();
 }
+
+// Add items to cart
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  foreach ($_POST['item'] as $item_id) {
+    if (!isset($_SESSION['cart'][$item_id])) {
+      $_SESSION['cart'][$item_id] = 1;
+    } else {
+      $_SESSION['cart'][$item_id]++;
+    }
+  }
+}
+
+// Retrieve cart items
+$cart_items = array();
+foreach ($_SESSION['cart'] as $item_id => $quantity) {
+  $item_query = "SELECT * FROM inventory WHERE productid='".$item_id."'";
+  $item_result = mysqli_query($con, $item_query);
+  $item_row = mysqli_fetch_assoc($item_result);
+  $item_row['quantity'] = $quantity;
+  $cart_items[] = $item_row;
+}
+
+// Display cart
+echo "<h1>Cart</h1>";
+echo "<table>";
+echo "<thead><tr><th>Item</th><th>Price</th><th>Quantity</th><th>Total</th></tr></thead>";
+echo "<tbody>";
+$total_price = 0;
+foreach ($cart_items as $item) {
+  $item_price = $item['price'] * $item['quantity'];
+  $total_price += $item_price;
+  echo "<tr>";
+  echo "<td>".$item['item_name']."</td>";
+  echo "<td>$".$item['price']."</td>";
+  echo "<td>".$item['quantity']."</td>";
+  echo "<td>$".$item_price."</td>";
+  echo "</tr>";
+}
+echo "</tbody>";
+echo "<tfoot><tr><td colspan='3'>Total:</td><td>$".$total_price."</td></tr></tfoot>";
+echo "</table>";
+
+// Checkout form
+echo "<h2>Checkout</h2>";
+echo "<form action='checkout.php' method='post'>";
+echo "<label for='payment_type'>Payment Type:</label>";
+echo "<select name='payment_type' id='payment_type'>";
+echo "<option value='cash'>Cash</option>";
+echo "<option value='card'>Card</option>";
+echo "</select>";
+echo "<br>";
+echo "<label for='to_go'>To Go:</label>";
+echo "<input type='checkbox' name='to_go' id='to_go'>";
+echo "<br>";
+echo "<label for='branchN'>Branch:</label>";
+echo "<select name='branchN' id='branchN'>";
+echo "<option value='1'>Branch 1</option>";
+echo "<option value='2'>Branch 2</option>";
+echo "</select>";
+echo "<br>";
+echo "<input type='submit' value='Checkout'>";
+echo "</form>";
 ?>
