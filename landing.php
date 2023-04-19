36053d105eab2cf191f779b567efebdc94886e10
Shn9 <53298173+SeanHungNguyen@users.noreply.github.com>
2023-04-19 09:57:46 -0500

    Update landing.php
    Image slider

--- landing.php
+++ landing.php
@@ -17,19 +17,61 @@
 
 <html>
   <head>
-  <title>Bean Me Up</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bean Me Up</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="landingstyle.css">
+    <style>
+      .background {
+        position: relative;
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+      }
+
+      .backimg {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        transition: opacity 1s ease-in-out;
+      }
+
+      .backimg.active {
+        opacity: 1;
+      }
+
+      .backimg.inactive {
+        opacity: 0;
+      }
+    </style>
   </head>
-  <div class="background">
-      <div class = "cont">
-          <button class = "button-1" onclick="window.location.href = '<?php echo $menuURL; ?>';">
-              View our menu
-          </button>
+  <body>
+    <div class="background">
+      <img class="backimg active" src="assets/coback.jpeg" alt="coffee image">
+      <img class="backimg active" src="assets/strawberry.jpeg" alt="latte image">
+      <img class="backimg active" src="assets/cookie1.jpeg" alt="snack image">
+      <div class="cont">
+        <button class="button-1" onclick="window.location.href = '<?php echo $menuURL; ?>';">
+          View our menu
+        </button>
       </div>
-      <img class = "backimg" src="assets/coback.jpeg" alt="coffe image">
-  </div>
+    </div>
+    <script>
+      const images = document.querySelectorAll('.backimg');
+      let index = 0;
+
+      setInterval(() => {
+        images[index].classList.remove('active');
+        images[index].classList.add('inactive');
+        index = (index + 1) % images.length;
+        images[index].classList.remove('inactive');
+        images[index].classList.add('active');
+      }, 3000);
+    </script>
+  </body>
 </html>
 
 <?php 
